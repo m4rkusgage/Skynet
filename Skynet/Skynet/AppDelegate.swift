8f79bec926f371453a315dae40730b409bb2c045
Markus Gage <markgage86@gmail.com>
2018-04-19 19:33:16 -0400

    setting up the AppCoordinator in the AppDelegate to start the application

--- Skynet/Skynet/AppDelegate.swift
+++ Skynet/Skynet/AppDelegate.swift
@@ -12,10 +12,19 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var coordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let navController = UINavigationController()
+        self.coordinator = AppCoordinator(navController: navController)
+        self.coordinator?.start()
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = navController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
