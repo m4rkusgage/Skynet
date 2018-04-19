7235a7b55a9eb0aaafa2133e44a41c69398ffe2a
Markus Gage <markgage86@gmail.com>
2018-04-19 16:06:18 -0400

    initial project setup

---
+++ Skynet/Skynet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skynet
+//
+//  Created by Markus Gage on 2018-04-19.
+//  Copyright © 2018 Mark Gage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
